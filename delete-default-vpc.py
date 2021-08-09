c921a24cf8a2cb2a8ad9183526f9a60323e0e1a0
Squwid <bwhitelaw24@gmail.com>
2021-08-09 12:58:37 -0400

    add changes to delete multiple accounts default vpcs

--- delete-default-vpc.py
+++ delete-default-vpc.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # Must be the first line
 from __future__ import print_function
+import urllib3
 
 import sys
 import json
 import boto3
+import time
 
 VERBOSE = 1
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def get_regions(client):
   """ Build a region list """
@@ -39,9 +42,20 @@ def get_default_vpcs(client):
   vpcs = json.loads(data)
   
   for vpc in vpcs:
-    vpc_list.append(vpc['VpcId'])  
+    tags = {tag['Key']: tag['Value'] for tag in vpc.get('Tags', [])}
+    vpc_name = tags.get('Name', 'No name')
+    vpc_list.append((vpc['VpcId'], vpc_name))
   
   return vpc_list
+
+def describe_nics(ec2, vpcid):
+  nics = ec2.describe_network_interfaces(
+    Filters=[
+      {"Name": "vpc-id", "Values": [vpcid]}
+    ]
+  )
+
+  return nics['NetworkInterfaces']
 
 def del_igw(ec2, vpcid):
   """ Detach and delete the internet-gateway """
@@ -58,7 +72,7 @@ def del_igw(ec2, vpcid):
           # DryRun=True
         )
       except boto3.exceptions.Boto3Error as e:
-        print(e)
+        raise e
 
 def del_sub(ec2, vpcid):
   """ Delete the subnets """
@@ -74,7 +88,7 @@ def del_sub(ec2, vpcid):
           # DryRun=True
         )
     except boto3.exceptions.Boto3Error as e:
-      print(e)
+      raise e
 
 def del_rtb(ec2, vpcid):
   """ Delete the route-tables """
@@ -93,7 +107,7 @@ def del_rtb(ec2, vpcid):
           # DryRun=True
         )
     except boto3.exceptions.Boto3Error as e:
-      print(e)
+      raise e
 
 def del_acl(ec2, vpcid):
   """ Delete the network-access-lists """
@@ -112,7 +126,7 @@ def del_acl(ec2, vpcid):
           # DryRun=True
         )
     except boto3.exceptions.Boto3Error as e:
-      print(e)
+      raise e
 
 def del_sgp(ec2, vpcid):
   """ Delete any security-groups """
@@ -129,7 +143,7 @@ def del_sgp(ec2, vpcid):
           # DryRun=True
         )
     except boto3.exceptions.Boto3Error as e:
-      print(e)
+      raise e
 
 def del_vpc(ec2, vpcid):
   """ Delete the VPC """
@@ -140,15 +154,49 @@ def del_vpc(ec2, vpcid):
       # DryRun=True
     )
   except boto3.exceptions.Boto3Error as e:
-      print(e)
       print("Please remove dependencies and delete VPC manually.")
-  #finally:
-  #  return status
+      raise e
 
-def main(keyid, secret):
+def main():
+  # List of account names to delete default vpcs of
+  # There must be an aws profile_name matching each of these account names
+  account_names = [
+    "test"
+  ]
+  dry_run = True # Change this to true to check for regions and nics in those regions
+
+  print(f'DryRun: {dry_run}. Sleeping for 5s')
+  time.sleep(5)
+
+  successful_accounts = []
+  unsuccessful_accounts = {}
+
+  for account_name in account_names:
+    output = delete_default_vpcs(account_name, dry_run)
+    if output == "Success":
+      successful_accounts.append(account_name)
+    else:
+      unsuccessful_accounts[account_name] = output
+  
+  # Print outputs
+  print('\n\n')
+  print(f'Full Deletions {len(successful_accounts)}/{len(account_names)}')
+  if len(unsuccessful_accounts) != 0:
+    print(f'Unsuccessful accounts')
+    for k, v in unsuccessful_accounts.items():
+      print(f'{k}: {v}')
+
+    
+
+# delete_default_vpcs takes an account name (also used for profile_name) and a dry run boolean and returns a status string
+# of what happened when trying to delete the default vpcs, whether it was successful or not
+def delete_default_vpcs(account_name, dry_run=True):
+  session = boto3.Session(profile_name=account_name)
+
+  print(f'\n\n*** Deleting Default VPCs for {account_name} - DryRun={dry_run} ***')
+
   """
   Do the work - order of operation
-
   1.) Delete the internet-gateway
   2.) Delete subnets
   3.) Delete route-tables
@@ -157,27 +205,69 @@ def main(keyid, secret):
   6.) Delete the VPC 
   """
 
-  client = boto3.client('ec2')
+  client = session.client('ec2', verify=False)
   regions = get_regions(client)
-  
+  sleep_time = 1 
   for region in regions:
+    print(f'Region {region}')
+
     try:
-      client = boto3.client('ec2', region_name = region)
-      ec2 = boto3.resource('ec2', region_name = region)
+      ec2 = session.resource('ec2', region_name = region, verify=False)
+      client = session.client('ec2', region_name = region, verify=False)
       vpcs = get_default_vpcs(client)
+      print(f'Got {len(vpcs)} default vpcs')
+
     except boto3.exceptions.Boto3Error as e:
-      print(e)
-      exit(1)
+      return f'Error getting vpcs {e}'
+
     else:
       for vpc in vpcs:
-        print("\n" + "\n" + "REGION:" + region + "\n" + "VPC Id:" + vpc)
-        del_igw(ec2, vpc)
-        del_sub(ec2, vpc)
-        del_rtb(ec2, vpc)
-        del_acl(ec2, vpc)
-        del_sgp(ec2, vpc)
-        del_vpc(ec2, vpc)
+        print("\n" + "\n" + "REGION:" + region + "\n" + "VPC Id:" + vpc[0] + "\n" + "VPC Name:" + vpc[1])
+
+        try:
+          nics = describe_nics(client, vpc[0])
+        except Exception as e:
+          return f'Error getting nics {e}'
+
+        for nic in nics:
+          print('VPC ID: ' + vpc[0] + " -- NIC: " + nic['NetworkInterfaceId'])
+
+        if len(nics) != 0:
+          print('TOO MANY NICS FOR VPC, CANT DELETE')
+          continue
+
+        
+        if dry_run:
+          print(f'DryRun={dry_run}. Would delete, skipping...') 
+        else:
+          print(f'DryRun={dry_run}. Deleting resources...')
+          try:
+            print("deleting igw")
+            del_igw(ec2, vpc[0])
+            time.sleep(sleep_time)
+            
+            print("deleting subs")
+            del_sub(ec2, vpc[0])
+            time.sleep(sleep_time)
+          
+            print("deleting rtbs")
+            del_rtb(ec2, vpc[0])
+            time.sleep(sleep_time)
+            
+            print("deleting acls")
+            del_acl(ec2, vpc[0])
+            time.sleep(sleep_time)
+            
+            print("deleting sgs")
+            del_sgp(ec2, vpc[0])
+            time.sleep(sleep_time)
+            
+            print("deleting vpc")
+            del_vpc(ec2, vpc[0])
+          except Exception as e:
+            return f'Error deleting things {e}'
+        
+        return 'Success'
 
 if __name__ == "__main__":
-
-  main(keyid = 'XXXX', secret = 'XXXX')
+  main()
